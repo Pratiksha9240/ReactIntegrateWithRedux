fe3137f0f644633af0ddd11e41a0635d6e1c6d6f
Pratiksha9240 <dhavalepratiksha9240@gmail.com>
2023-03-02 13:43:53 +0530

    Toggle action added

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -2,11 +2,15 @@ import classes from "./Counter.module.css";
 import { useDispatch, useSelector } from "react-redux";
 
 const Counter = () => {
-  const toggleCounterHandler = () => {};
 
   const dispatch = useDispatch();
 
   const counter = useSelector((state) => state.counter);
+  const show = useSelector(state => state.showCounter);
+
+  const toggleCounterHandler = () => {
+    dispatch({type:'toggle'})
+  };
 
   const incrementHandler = () => {
     dispatch({ type: "increment" });
@@ -27,7 +31,7 @@ const Counter = () => {
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {show && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={incrementHandler}>Increment</button>
         <button onClick={decrementHandler}>Decrement</button>
